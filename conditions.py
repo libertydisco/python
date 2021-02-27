af0277babbe146cf47b71f060e4b7b287df5a0d1
Matthew Barabash <matthew.barabash@alumni.utoronto.ca>
2021-02-27 15:28:37 -0500

    first commit

---
+++ conditions.py
@@ -0,0 +1,7 @@
+num = int(input("Number: "))
+if num > 0:
+    print("Mr. T says your number is positive, foo.")
+elif num < 0:
+    print("Mr. T says your number is negative, foo.")
+else:
+    print("Mr. T says your number is 0, foo")
